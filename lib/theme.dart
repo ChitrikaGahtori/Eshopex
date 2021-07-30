fed9207a51411b662bbd5014fd1075d4c541b001
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-07-30 07:38:42 +0530

    AppBar Theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,6 +5,19 @@ ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
     fontFamily: "Muli",
+    appBarTheme: appBarTheme(),
     visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return AppBarTheme(
+    color: Colors.white,
+    elevation: 0.0,
+    brightness: Brightness.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    textTheme: TextTheme(
+      headline6: TextStyle(color: Colors.black, fontSize: 22),
+    ),
   );
 }
