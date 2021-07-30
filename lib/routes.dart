2979a8f4d8d45ca8739741541dd3b78b5bdf0126
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-07-30 07:20:39 +0530

    Adding signIn route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
+import 'package:e_shopex/screens/sign_in/sign_in_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:e_shopex/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
 };
 
