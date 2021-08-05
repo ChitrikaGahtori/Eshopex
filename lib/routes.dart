a6aae4bf74c54746d2e6771784d5c2cc3ec40c9a
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-08-05 09:16:27 +0530

    Otp route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,15 @@
+import 'package:e_shopex/screens/forgot_password/forgot_password_screen.dart';
+import 'package:e_shopex/screens/otp/otp_screen.dart';
 import 'package:e_shopex/screens/sign_in/sign_in_screen.dart';
+import 'package:e_shopex/screens/sign_up/sign_up_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:e_shopex/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
 };
 
