1844804f3bb1f86db0bddd2274d49cd470e8c155
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-08-05 09:15:22 +0530

    Set Decoration Theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
     fontFamily: "Muli",
     appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
@@ -19,5 +23,26 @@ AppBarTheme appBarTheme() {
     textTheme: TextTheme(
       headline6: TextStyle(color: Colors.black, fontSize: 22),
     ),
+  );
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: BorderSide(color: kTextColor),
+    gapPadding: 7,
+  );
+  return InputDecorationTheme(
+    contentPadding: EdgeInsets.symmetric(horizontal: 38, vertical: 18),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
   );
 }
