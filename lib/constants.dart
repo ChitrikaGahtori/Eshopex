e313c63ea94b11cd23c7effa9a5e38a69505f184
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-08-05 09:20:26 +0530

    Otp textfield input

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
+import 'package:e_shopex/size_config.dart';
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Colors.blueAccent;
@@ -12,3 +13,31 @@ const kTextColor = Color(0xFF757575);
 
 const kAnimationDuration = Duration(milliseconds: 200);
 
+final RegExp emailValidatorRegExp =
+RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNameNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+  EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+  counter: SizedBox(
+    width: 0,
+    height: 0,
+  ),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
+}
