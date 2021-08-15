16f42651d6b1e9c677aa4524fd0185eb86b2703d
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-08-16 01:23:53 +0530

    Home header and Search UI

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:e_shopex/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = "/home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+      drawer: Drawer(),
+    );
+  }
+}
