8d19ffd434f2104d4baf7a3056f22d46fa3930fb
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2021-08-18 07:36:06 +0530

    title decoration

---
+++ lib/screens/home/components/section_title.dart
@@ -0,0 +1,38 @@
+import 'package:e_shopex/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+
+  const SectionTitle({
+    Key key,
+    @required this.title,
+    @required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(18),
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        GestureDetector(
+          onTap: press,
+          child: Text(
+            "See More",
+            style: TextStyle(color: Color(0xFF535353)),
+          ),
+        ),
+      ],
+    );
+  }
+}
